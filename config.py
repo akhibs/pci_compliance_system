ec16a3fd2aea728ae0bf21f6ff82b3112b8e9893
Your Paul <akigbepaul555@gmail.com>
2026-06-02 00:39:26 +0100

    new

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'pci-compliance-secret-key-2024'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///compliance.db"
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'pci-compliance-secret-key-2024')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "sqlite:///compliance.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_ENV = os.environ.get('FLASK_ENV', 'production')
